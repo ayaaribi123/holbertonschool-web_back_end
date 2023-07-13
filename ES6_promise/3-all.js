3934a0c0f92ccec54d1b0318fa4ec7d85fa67e25
ayaaribi123 <ayaaribi11@gmail.com>
2023-07-13 14:37:07 +0100

    ES6 promise

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,9 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
-      const { body } = result;
-      const { firstName } = result;
-      const { lastName } = result;
+      const { body } = result[0];
+      const { firstName } = result[1];
+      const { lastName } = result[2];
       console.log(`${body} ${firstName} ${lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
