022eb8f75b11d7a0df03e867e72d87fb8c67f0ef
ayaaribi123 <ayaaribi11@gmail.com>
2023-07-14 14:06:48 +0100

    ES6 promise

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -2,7 +2,5 @@ export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw Error('cannot divide by 0');
   }
-  else {
-    return numerator / denominator;
-  }
+  return numerator / denominator;
 }
