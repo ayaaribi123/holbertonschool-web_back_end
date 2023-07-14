ffe01535a3b4d8e6ec1a9b916d17b1423c218ebe
ayaaribi123 <ayaaribi11@gmail.com>
2023-07-14 19:02:11 +0100

    ES6 classes

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,31 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+		
+  set length(length) {
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  set students(students) {
+    if (typeof students === 'string'){
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+}
