76c1dd8916a677c4b99d6a9b23d437f032ed45c4
ayaaribi123 <ayaaribi11@gmail.com>
2023-07-25 14:47:56 +0100

    ES6 classes

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,30 +1,30 @@
 export default class Currency {
-	constructor(code, name) {
-			this.code = code;
-			this.name = name;
-	}
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
 
-	get code() {
-			return this._code;
-	}
+  get code() {
+    return this._code;
+  }
 
-	set code(code) {
-			if (typeof code === 'string'){
-    this._code = code;
-			}
-	}
+  set code(code) {
+    if (typeof code === 'string'){
+      this._code = code;
+    }
+  }
 
-	get name() {
-			return this._name;
-	}
+  get name() {
+    return this._name;
+  }
 
-	set name(name) {
-			if (typeof name === 'string'){
-    this._name = name;
-			}
-	}
+  set name(name) {
+    if (typeof name === 'string'){
+      this._name = name;
+    }
+  }
 
-	displayFullCurrency() {
-			return `${this.name} (${this.code})`;
-	}
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
 }
