160593b0ea204ee32982427f8bceaa24a5077856
ayaaribi123 <ayaaribi11@gmail.com>
2023-07-25 16:01:58 +0100

    ES6 classes

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,8 +2,9 @@ export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building
       && typeof this.evacuationWarningMessage !== 'function'
-    ) {throw Error(
-       'Class extending Building must override evacuationWarningMessage',
+    ) {
+     throw Error(
+   'Class extending Building must override evacuationWarningMessage',
       );
     }
 
