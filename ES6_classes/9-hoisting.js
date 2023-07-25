a6fcd4d5dd3970c74e90380bc0f60eb44550be59
ayaaribi123 <ayaaribi11@gmail.com>
2023-07-25 18:51:30 +0100

    ES6 classes

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -20,8 +20,8 @@ const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+export default listOfStudents;
 export const listOfStudents = [student1, student2, student3, student4, student5];
-
 export class StudentHolberton {
   constructor(firstName, lastName) {
     this._firstName = firstName;
