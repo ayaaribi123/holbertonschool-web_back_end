02f7c40019ddbdd2211095819cd5f118e8158c85
ayaaribi123 <ayaaribi11@gmail.com>
2023-07-25 14:01:04 +0100

    ES6 classes

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -21,13 +21,13 @@ export default class HolbertonCourse {
     }
   }
 
-	set students(students) {
-			if ( Array.isArray(students)
-					&& students.every((s) => typeof s === 'string')
-			) {
-					this._students = students;
-			} else {
-					throw new TypeError('Students must be an array of strings');
-			}
-	}
+  set students(students) {
+    if ( Array.isArray(students)
+				    && students.every((s) => typeof s === 'string')
+    ) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
 }
