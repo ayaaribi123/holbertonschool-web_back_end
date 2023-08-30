985026366182482fae7627b5418a540703bd7302
ayaaribi123 <ayaaribi11@gmail.com>
2023-08-30 13:14:12 +0100

    NODE JS

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -3,25 +3,24 @@ const fs = require('fs');
 function countStudents(path) {
 	let content;
 
-	try {
-		content = fs.readFileSync(path);
-	} catch (err) {
-		throw new Error('Cannot load the database');
-	}
+  try {
+		  content = fs.readFileSync(path);
+	 } catch (err) {
+		  throw new Error('Cannot load the database');
+  }
 	content = content.toString().split('\n');
 
+  let student = content.filter((item) => item);
 
-	let student = content.filter((item) => item);
+  student = student.map((item) => item.split(','));
 
-	student = student.map((item) => item.split(','));
+  const number= student.length ? student.length - 1 : 0;
+  console.log(`Number of students: ${number}`);
 
-	const number= student.length ? student.length - 1 : 0;
-	console.log(`Number of students: ${number}`);
-
-	const field = {};
-	for (const i in student) {
-		if (i !== 0) {
-			if (!field[student[i][3]]) field[student[i][3]] = [];
+  const field = {};
+  for (const i in student) {
+    if (i !== 0) {
+    if (!field[student[i][3]]) field[student[i][3]] = [];
 
 			field[student[i][3]].push(student[i][0]);
 		}
