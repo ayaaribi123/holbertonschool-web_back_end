475156add24604b404ba3a38503380379f2ecabe
ayaaribi123 <ayaaribi11@gmail.com>
2023-08-30 13:20:21 +0100

    NODE JS

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,35 +1,35 @@
 const fs = require('fs');
 
 function countStudents(path) {
-	let content;
+  let content;
 
   try {
-		  content = fs.readFileSync(path);
-	 } catch (err) {
-		  throw new Error('Cannot load the database');
+    content = fs.readFileSync(path);
+  } catch (err) {
+    throw new Error('Cannot load the database');
   }
-	content = content.toString().split('\n');
+  content = content.toString().split('\n');
 
   let student = content.filter((item) => item);
 
   student = student.map((item) => item.split(','));
 
-  const number= student.length ? student.length - 1 : 0;
+  const number = student.length ? student.length - 1 : 0;
   console.log(`Number of students: ${number}`);
 
   const field = {};
   for (const i in student) {
     if (i !== 0) {
-    if (!field[student[i][3]]) field[student[i][3]] = [];
+      if (!field[student[i][3]]) field[student[i][3]] = [];
 
-			field[student[i][3]].push(student[i][0]);
-		}
-	}
+      field[student[i][3]].push(student[i][0]);
+		  }
+	 }
 
-	delete field.field;
+	 delete field.field;
 
-	for (const key of Object.keys(field)) {
-		console.log(
+	 for (const key of Object.keys(field)) {
+		  console.log(
 			`Number of students in ${key}: ${field[key].length}. List: ${field[
 				key
 			].join(', ')}`,
