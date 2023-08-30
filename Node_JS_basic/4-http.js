92480d5a962f3fe62ddac5c0ce3e28bbaec9834e
ayaaribi123 <ayaaribi11@gmail.com>
2023-08-30 15:02:07 +0100

    NODE JS

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,12 +1,8 @@
 const http = require('http');
 
-const hostname = '124.5.0.0';
-const port = 1245;
-
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
-
 module.exports = app;
