00cf52b287221898793f6af99859a00a0eb84a6b
ayaaribi123 <ayaaribi11@gmail.com>
2023-09-02 19:33:37 +0100

    data manipulation

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,15 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-	return students.filter((student) => student.location === city).map((student) => {
-			const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id);
-			let	grades
+  return students.filter((student) => student.location === city).map((student) => {
+    const grade = newGrades.filter((newGrade) => newGrade.studentId === student.id);
+    let	grades;
 
-			if (grade[0]) {
-				grades = grade[0].grades;
-			} else {
-					grades = 'N/A';
-			}
+    if (grade[0]) {
+      grades = grade[0].grades;
+    } else {
+      grades = 'N/A';
+    }
 
-			return {
-					...student, grades
-			};
-	});
+    return {
+      ...student, grades };
+  });
 }
