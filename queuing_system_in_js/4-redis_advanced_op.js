a171a70db2be42aea72b82b0a2567cc4c3ba4155
ayaaribi123 <ayaaribi11@gmail.com>
2023-09-02 22:08:22 +0100

    queuing system

---
+++ queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,22 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+const keys = ['Portland', 'Seattle', 'New York', 'Bogota', 'Cali', 'Paris'];
+const values = [50, 80, 20, 20, 40, 2];
+
+keys.forEach((key, index) => {
+	client.hset('HolbertonSchools', key, values[index], redis.print);
+});
+
+client.hgetall('HolbertonSchools', (err, value) => {
+  console.log(value);
+});
