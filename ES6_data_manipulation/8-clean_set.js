8806f7fcb4f6dd415f5a67ee63f729269e807a43
ayaaribi123 <ayaaribi11@gmail.com>
2023-09-02 20:42:52 +0100

    data manipulation

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,14 +1,14 @@
 export default function cleanSet(set, startString) {
-	if (
-			typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0
-	) {
-			return '';
-	}
+  if (
+    typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0
+  ) {
+    return '';
+  }
   const arr = [];
-	for (const item of set) {
-			if (item && item.startsWith(startString)) {
-					arr.push(item.slice(startString.length));
-			}
-	}
-	return arr.join('-');
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      arr.push(item.slice(startString.length));
+    }
+  }
+  return arr.join('-');
 }
