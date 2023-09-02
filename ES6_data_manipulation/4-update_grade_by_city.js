e5505ebf7f21a46ceddc7f1c189e49de4a8c3eba
ayaaribi123 <ayaaribi11@gmail.com>
2023-09-02 18:45:08 +0100

    data manipulation

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+	return students.filter((student) => student.location === city).map((student) => {
+			const grade = newGrades.filter((grade) => grade.studentId === student.id);
+
+			if (grade[0]) {
+				var	grades = grade[0].grades;
+			} else {
+					grades = 'N/A';
+			}
+
+			return {
+					...student, grade
+			};
+	});
+}
