9e962aca76574ce9e8d1e67b93236754e23bc65c
ayaaribi123 <ayaaribi11@gmail.com>
2023-09-02 15:53:41 +0100

    NODE JS

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,26 @@
+const http = require('http');
+
+const args = process.argv.slice(2);
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(args[0]);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+});
+
+module.exports = app;
